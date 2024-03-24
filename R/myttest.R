a70be84f3d6bac37f145d399f1d6ff8de8b8b5e0
nurfnick <njacob@ecok.edu>
2024-03-24 15:37:34 -0500

    created and knit part 1

--- R/myttest.R
+++ R/myttest.R
@@ -2,9 +2,9 @@
 #'
 #' @param x vector of data
 #' @param y vector of data
-#' @param alpha signifigance level between 0 and 1
+#' @param alpha significance level between 0 and 1 5% by default
 #'
-#' @return
+#' @return list that include original data, alpha, ci, and pvalue
 #' @export
 #'
 #' @examples
@@ -16,16 +16,15 @@
 #'  print(obj)
 
 myttest <-function(x,y,alpha = 0.05){
-  tt<-t.test(x,y,var.equal = TRUE, conf.level=1-alpha)
-  data <- vector(mode = "numeric", length = length(x) + length(y))
-  v <- vector(mode = "list", length = length(data))
-
-  v <- rep(c("x","y"), c(length(x),length(y)))
-  data <- c(x,y)
-  df = data.frame("data" = data, "v" = v)
-  confInt <- c(tt[["conf.int"]][1],tt[["conf.int"]][2])
-  pval <- c(tt[["p.value"]])
-  lst <- list("df" = df, "alpha" = alpha, "confidenceInterval" = confInt, "pvalue" = pval)
+  data <- vector(mode = "numeric", length = length(x) + length(y)) #setting up the data for later
+  v <- vector(mode = "list", length = length(data))#getting a list of which variable is which
+  tt<-t.test(x,y,var.equal = TRUE, conf.level=1-alpha) #do the test
+  v <- rep(c("x","y"), c(length(x),length(y))) #which value is which
+  data <- c(x,y) #add the values
+  df = data.frame("data" = data, "v" = v) #add everything to the df
+  confInt <- c(tt[["conf.int"]][1],tt[["conf.int"]][2]) #get both values of the confidence interval
+  pval <- c(tt[["p.value"]]) #get the p value
+  lst <- list("df" = df, "alpha" = alpha, "confidenceInterval" = confInt, "pvalue" = pval) #add it all to a list
   class(lst)<-"Rttest"# New class
-  lst
+  lst #return the list
 }
