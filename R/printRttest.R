e7f9e87b593662ba16804116bce16b70cfcd8279
nurfnick <njacob@ecok.edu>
2024-03-26 16:58:15 -0500

    added print.  Still not sure it is working...

--- R/printRttest.R
+++ R/printRttest.R
@@ -1,14 +1,17 @@
-#' Print the Rttest Object
+#' @title Print the Rttest Object
 #'
 #' @param x Rttest object
 #' @param ... needed for print to work
 #'
 #' @return
+#'
+#' @importFrom kableExtra kable
+#'
 #' @export print.Rttest
 #'
 #' @export
 #'
 #' @examples
 print.Rttest <- function(x, ...){
-  print(x$df)
+  kable(x$df, padding = 5, digits = 4, caption = "Data From t-Test")
 }
